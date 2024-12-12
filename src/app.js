b83054102c4816060e9bf0c67103b104b6320286
VinayNoogler000 <vinaytambey000@gmail.com>
2024-12-12 23:14:27 +0530

    Feat - Implemented functionality to calculate and update earnings, expenses and balance

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 // ------------------------ Selecting HTML-DOM Elements -----------------------
-const transContEl = document.querySelector(".transactions-container"); // transaction-container-element
-const formEl = document.querySelector(".transaction-form");
+const transactionContEl = document.querySelector(".transactions-container"); // transaction-container-element
+const transactionFormEl = document.querySelector(".transaction-form"); //transaction-form-element
 const sourceInpEl = document.querySelector("#sourceInp");
 const amountInpEl = document.querySelector("#amountInp");
-const earningsBtnEl = document.querySelector("#earningsBtn");
-const expensesBtnEl = document.querySelector("#expensesBtn");
+const earningsEl = document.querySelector("#earnings");
+const expensesEl = document.querySelector("#expenses");
+const balanceEl = document.querySelector("#balance");
 
 // ------------ Function to Create & Display `.transactions` element -----------
-const addTransactionEl = (source, amount, buttonText) => {
+const addTransactionEl = (source, amount, transactionCategory) => {
   const transactionEl = document.createElement("div");
   transactionEl.className = "transactions";
 
@@ -24,40 +25,66 @@ const addTransactionEl = (source, amount, buttonText) => {
 
   const amountEl = document.createElement("span");
   amountEl.className = "amount";
-  amountEl.textContent = buttonText.toLowerCase() === "earnings" ? "+ ₹" + amount : "- ₹" + amount;
+  amountEl.textContent = transactionCategory === "earnings" ? "+ ₹" + amount : "- ₹" + amount;
 
-  const transTypeEl = document.createElement("span"); // transaction-type-element
-  transTypeEl.className = "type";
+  const transactionTypeEl = document.createElement("span"); // transaction-type-element
+  transactionTypeEl.className = "type";
 
-  if (buttonText.toLowerCase() === "earnings") {
-    transTypeEl.classList.add("credit");
-    transTypeEl.textContent = "C";
+  if (transactionCategory === "earnings") {
+    transactionTypeEl.classList.add("credit");
+    transactionTypeEl.textContent = "C";
   } else {
-    transTypeEl.classList.add("debit");
-    transTypeEl.textContent = "D";
+    transactionTypeEl.classList.add("debit");
+    transactionTypeEl.textContent = "D";
   }
-  amountInfoEl.append(amountEl, transTypeEl);
+  amountInfoEl.append(amountEl, transactionTypeEl);
 
-  transContEl.appendChild(transactionEl); // add the transactionEl on the webpage
+  transactionContEl.appendChild(transactionEl); // add the transactionEl on the webpage
 };
 
-// ----------------------- Define Event Listeners for Elements --------------------
-let clickedBtn; // used to track the clicked button, either earnings-btn or expenses-btn
+// ----- Auxillary Function to Calculate & Update Earnings, Expenses and Balance --------
+const calculateEarnings = () => {
+  totalEarnings += Number(amountInpEl.value);
+  earningsEl.textContent = "₹" + totalEarnings;
+};
 
-formEl.addEventListener("submit", (event) => {
+const calculateExpenses = () => {
+  totalExpenses += Number(amountInpEl.value);
+  expensesEl.textContent = "₹" + totalExpenses;
+};
+
+const calculateBalance = () => {
+  balance = totalEarnings - totalExpenses;
+  balanceEl.textContent =
+    balance >= 0 ? "₹" + balance : "- ₹" + Math.abs(balance);
+};
+
+// ---------------- Function to Calculate Earnings, Expenses, and Balance -----------------
+const calculateFinancialVariables = (transactionCategory) => {
+  if (transactionCategory === "earnings") {
+    calculateEarnings();
+  } else {
+    calculateExpenses();
+  }
+
+  calculateBalance();
+};
+
+// ---------------------------- Initialize Variables as Trackers ----------------------------
+let totalEarnings = 0;
+let totalExpenses = 0;
+let balance;
+
+// ----------------------- Define Event Listeners & Handlers for Elements --------------------
+transactionFormEl.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  let buttonText = clickedBtn.children[2].innerText; // get button text - "earnings" or "expenses"
+  let clickedBtn = event.submitter; // used to track the clicked button, either earnings-btn or expenses-btn
+  let buttonText = clickedBtn.children[2].innerText.toLowerCase(); // get button text - "earnings" or "expenses"
+
   addTransactionEl(sourceInpEl.value, amountInpEl.value, buttonText);
+  calculateFinancialVariables(buttonText);
 
   // Empty the Input-Fields:
   sourceInpEl.value = amountInpEl.value = "";
-});
-
-earningsBtnEl.addEventListener("click", () => {
-  clickedBtn = earningsBtnEl;
-});
-
-expensesBtnEl.addEventListener("click", () => {
-  clickedBtn = expensesBtnEl;
 });
